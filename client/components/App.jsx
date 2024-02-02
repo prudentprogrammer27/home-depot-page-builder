8130f8edbd29003f36f469e8aec921b521de2dee
Heather Swinn <h.swinn@Heathers-MacBook-Air.local>
2024-02-02 10:01:54 -0800

    accordion works as intended, need to add content

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
+import Accordion from "./Accordion";
 
 const App = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <main>
+      <div className="accordionGrid">
+        <Accordion />
+      </div>
     </main>
   );
 };
