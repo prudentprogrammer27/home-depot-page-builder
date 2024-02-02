fdab8c3f58524b2eed9866197d9dda31cb656198
derekmanninghm <derekmanninghm@gmail.com>
2024-02-02 14:54:32 -0800

    sidebar minimum display reqs

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from "react";
+import Sidebar from "./SidebarJSX/Sidebar.jsx";
 
 const App = () => {
 
   return (
     <main>
-      <div></div>
+      <header><span>HEADER</span></header>
+      <div className="item-wrapper">
+        <div className="item-wrapper bounding-box">
+          <div className="link-path">Link path // store sku</div>
+          <div className="picture-and-sidebar">
+            <div className="img-gallery"><span>GALLERY</span></div>
+            <Sidebar/>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
---
+++ client/components/SidebarJSX/ItemFacts.jsx
@@ -0,0 +1,14 @@
+const ItemFacts =()=> {
+    return (
+        <div className="item-facts">
+            <ul>
+                <li>Up to 25% Faster Driving and Up to 50% More Torque</li>
+                <li>{'Includes: (2) 2.0 Ah High Performance Batteries, Charger, and Bag'}</li>
+                <li>{'Over 300 ONE+ Products – All Work With Any RYOBI ONE+ 18V Battery'}</li>
+                <li><a href="daa">View More Details</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default ItemFacts;
