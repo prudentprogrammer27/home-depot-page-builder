2e402ecca643dbb4cc971de8dcf081956019922a
prudentprogrammer27 <declercq008@gmail.com>
2024-02-02 17:18:02 -0700

    HTML turned mostly React component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-
+import Header from './Header/Header.jsx'
 const App = () => {
 
   return (
     <main>
+      <Header/>
     </main>
   );
 };
