4e8f2b0f65e92ace1d8a92011578865ada6f7592
prudentprogrammer27 <declercq008@gmail.com>
2024-02-06 16:46:34 -0700

    Q&A component divs skeleton

--- client/components/Accordion/QandAContent.jsx
+++ client/components/Accordion/QandAContent.jsx
@@ -1,10 +1,108 @@
-function QandAContent() {
-    return (
-      <div>
-        {/* Specific content and functionality for Product Details panel */}
-        <p>This is where Q&A details go.</p>
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './QandAContent.css'
+
+const QandAContent = ({ currentProduct }) => {
+  const [ questions, setQuestions ] = useState([])
+
+  useEffect(() => {
+    const fetchQA = async () => {
+      try {
+        const response = await axios.get(`/api/customer_questions/${currentProduct.id}`);
+        setQuestions(response.data);
+      } catch (error) {
+        console.error('Error fetching data', error);
+      }
+    };
+
+    fetchQA();
+  }, [currentProduct]);
+
+ return (
+      <div id="accordion-qa">
+        {/* QA HEADER */}
+        <div id="qaHeaderCont">
+          <div id="qaHeaderSlim">
+            <div id="qaHeaderLeft">
+              <div id="qaHeaderLeftSub">
+                <div id="qaHeaderLeftSubLeft"></div>
+                <div id="qaHeaderLeftSubRight"></div>
+              </div>
+            </div>
+              <div id="qaHeaderMiddle"></div>
+              <div id="qaHeaderRight">
+                <div id="qaHeaderRightCont">
+                  <div id="qaHeaderRightBtnBox">
+                    <div id="qaHeaderRightText"></div>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+        {/* NAVIGATION BAR*/}
+        <div id="qaNavCont">
+          <div id="qaNavSlim">
+            <div id="qaNavLeft">
+              <div id="qaNavLeftSub"></div>
+            </div>
+            <div id="qaNavRight">
+              <div id="qaNavRightDropDown"></div>
+            </div>
+          </div>
+        </div>
+        {/* QUESTION DISPLAY */}
+        <div id="qaQuestionCont">
+          <div id="qaQuestionLeft">
+            <div id="qaQuestionLeftTop">
+              <div id="qaQuestionLeftTopQuestion"></div>
+            <div id="qaQuestionLeftBottom">
+              <div id="qaQuestionLeftBottomSub"></div>
+            </div>
+            </div>
+          </div>
+          <div id="qaQuestionRight">
+            <div id="qaQuestionRightSub">
+              <div id="qaQuestionRightCount"></div>
+            </div>
+          </div>
+        </div>
+       
+        {/* ANSWER BOX (switch statement) */}
+        <div id="qaAnswerCont">
+          <div id="qaAnswerTop">
+            <div id="qaAnswerTopLeft">
+              <div id="qaAnswerTopLeftTop"></div>
+              <div id="qaAnswerTopLeftBottom">
+                <div id="qaAnswerTopLeftBottomLeft"></div>
+              </div>
+              <div id="qaAnswerTopRight"></div>
+              <div id="qaAnswerBottom">
+                <div id="qaAnswerBottomLeft"></div>
+                <div id="qaAnswerBottomRight"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Q&A FOOTER */}
+        <div id="qaDropDownFooter">
+          <div id="qaDropDownFooterSub">
+            <div id="qaDropDownFooterSubTop"></div>
+            <div id="qaDropDownFooterBottom"></div>
+          </div>
+        </div>
+
+        {/* <h2>Q & A:</h2>
+        <ul>
+          {questions.map((questions) => (
+            <li className="QandA" key={questions.question_content}>
+              {questions.response_content}
+            </li>
+          ))}
+        </ul> */}
+
+       
       </div>
     );
-}
-  
+};
+
 export default QandAContent;
--- client/components/Header/Header.jsx
+++ client/components/Header/Header.jsx
@@ -10,11 +10,11 @@ const Header = () => {
         <div id="header-root">
           <div className="header-content">
             <div className="side-by-side" id="home-depot-logo">
-              <img src="./header-images/HDLogo.png" alt="Home Depot Logo" />
+              <img src="./components/Header/header-images/HDLogo.png" alt="Home Depot Logo" />
             </div>
             <div className="side-by-side" id="store-loc-time-zip">
               <div className="location-divs" id="storeLogo">
-                <img id="storePic" src="./header-images/store.png" alt="Store Icon" />
+                <img id="storePic" src="./components/Header/header-images/store.png" alt="Store Icon" />
               </div>
               <div className="location-divs" id="storeLocation">
                 SW Color...
@@ -23,7 +23,7 @@ const Header = () => {
                 9PM
               </div>
               <div className="location-divs" id="truck">
-                <img id="truckPic" src="./header-images/truck.png" alt="Truck Icon" />
+                <img id="truckPic" src="./components/Header/header-images/truck.png" alt="Truck Icon" />
               </div>
               <div className="location-divs" id="storeZip">
                 86753
@@ -33,21 +33,21 @@ const Header = () => {
               <input
                 id="searchStuff"
                 type="text"
-                id="searchInput"
+                // id="searchInput"
                 placeholder="What can we help you find today?"
               />
               <button onClick={() => performSearch()}>
-                <img src="./header-images/search.png" alt="Search Icon" />
+                <img src="./components/Header/header-images/search.png" alt="Search Icon" />
               </button>
             </div>
             <div className="side-by-side" id="navIcons">
-              <img src="./header-images/bucket.png" alt="Bucket Icon" />
-              <img src="./header-images/services.png" alt="Services Icon" />
-              <img src="./header-images/DIY.png" alt="DIY Icon" />
-              <img src="./header-images/me.png" alt="Me Icon" />
+              <img src="./components/Header/header-images/bucket.png" alt="Bucket Icon" />
+              <img src="./components/Header/header-images/services.png" alt="Services Icon" />
+              <img src="./components/Header/header-images/DIY.png" alt="DIY Icon" />
+              <img src="./components/Header/header-images/me.png" alt="Me Icon" />
             </div>
             <div className="side-by-side" id="cartIcon">
-              <img src="./header-images/cart.png" alt="Cart Logo" />
+              <img src="./components/Header/header-images/cart.png" alt="Cart Logo" />
               <div>3</div>
             </div>
           </div>
