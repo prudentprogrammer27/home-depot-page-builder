fbe77b755f99b9bed3ff888355c5d14f64941aab
derekmanninghm <derekmanninghm@gmail.com>
2024-02-08 14:14:32 -0800

    merged and deconflicted sidebar, sidebar-modal, and nav-bar branch. + minor css change

--- client/components/SidebarJSX/FulfillmentTiles.jsx
+++ client/components/SidebarJSX/FulfillmentTiles.jsx
@@ -7,8 +7,8 @@ const FulfillmentTiles = ({localStoreInfo, onlineStoreInfo}) => {
     return (
         <div className="fulfillment-tiles sb-parent">
             <div className="fulfillment-tiles fulfillment-headers">
-                <div className="fulfillment-header-pickup">Pickup at {localStoreInfo.city_name}</div>
-                <div className="fulfillment-header-delivery">Delivering to {localStoreInfo.zipcode}</div>
+                <div className="fulfillment-header-pickup">Pickup at <span>{localStoreInfo.city_name}</span></div>
+                <div className="fulfillment-header-delivery">Delivering to&thinsp;<span>{localStoreInfo.zipcode}</span></div>
             </div>
             <div className="cart-info fulfillment-cards">
                 <FulfillmentTile currTile={currTile} setCurrTile={setCurrTile} title={'Pickup'} sub1={'Today'} sub2={localStoreInfo.inventory} sub3={'in stock'}/>
